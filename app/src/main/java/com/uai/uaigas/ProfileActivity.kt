ccc9b3b8d1adb446218a2f009f0b64c9a0cc23b8
Gledson Junior <49466350+GJotaJunior@users.noreply.github.com>
2020-12-22 23:36:10 -0300

    Atualizando imagem e salvando no firebase

--- app/src/main/java/com/uai/uaigas/ProfileActivity.kt
+++ app/src/main/java/com/uai/uaigas/ProfileActivity.kt
@@ -5,21 +5,32 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class ProfileActivity : AppCompatActivity() {
 
     lateinit var name: TextInputLayout
     lateinit var email: TextInputLayout
     lateinit var userPhoto: ImageView
+    lateinit private var storageRef: StorageReference
     private val pickImage = 100
     private var imageUri: Uri? = null
 
+    //    Mocks
+    private var idMock = 3
+    private var nameMock = "Alfredo Borges Santana"
+    private var emailMock = "alfredo.b.s@uaigas.com"
+    private var photoUrlMock =
+        "https://image.freepik.com/free-vector/portrait-african-american-woman-profile-avatar-young-black-girl_102172-418.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,21 +40,15 @@ class ProfileActivity : AppCompatActivity() {
         email = findViewById(R.id.email)
         userPhoto = findViewById(R.id.user_photo)
 
-        var nameMock = "Alfredo Borges Santana"
-        var emailMock = "alfredo.b.s@uaigas.com"
-        var photoUrlMock =
-            "https://image.freepik.com/free-vector/portrait-african-american-woman-profile-avatar-young-black-girl_102172-418.jpg"
-        chargeProfileInfo(nameMock, emailMock, photoUrlMock)
-        changeUserPhoto(photoUrlMock)
-    }
+        storageRef = FirebaseStorage.getInstance().getReference("users/${idMock}")
+        updatePhotoUrl("https://firebasestorage.googleapis.com/v0/b/${storageRef.bucket}/o/users%2F${idMock}?alt=media");
 
-    fun chargeProfileInfo(name: String, email: String, photoUrl: String) {
-        this.name.editText?.setText(name)
-        this.email.editText?.setText(email)
-        changeUserPhoto(photoUrl)
+        chargeProfileInfo(nameMock, emailMock, photoUrlMock)
+        chargeUserPhoto(photoUrlMock)
     }
 
     fun updateProfileInfo(view: View) {
+//        TODO atualizar no backend
         val password = findViewById<TextInputLayout>(R.id.password).editText?.text
 
         val validator = !password.toString().isNullOrEmpty()
@@ -64,15 +69,38 @@ class ProfileActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == pickImage) {
             imageUri = data?.data
-            imageUri?.let { changeUserPhoto(it) }
+            imageUri?.let {
+                storageRef.putFile(it).addOnSuccessListener { taskSnapshot ->
+                    Toast.makeText(this, "Foto alterada!", Toast.LENGTH_SHORT).show()
+                    photoUrlMock =
+                        "https://firebasestorage.googleapis.com/v0/b/${storageRef.bucket}/o/users%2F${idMock}?alt=media)"
+                    chargeUserPhoto(it)
+                }.addOnFailureListener {
+                    Log.d("Erro!", it.toString())
+                    Toast.makeText(this, "Erro! Tente novamente", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
-    private fun changeUserPhoto(uri: Uri) {
-        Glide.with(this).load(uri).error(R.drawable.default_avatar).centerCrop().into(userPhoto)
+    private fun chargeProfileInfo(name: String, email: String, photoUrl: String) {
+        this.name.editText?.setText(name)
+        this.email.editText?.setText(email)
+        chargeUserPhoto(photoUrl)
     }
 
-    private fun changeUserPhoto(path: String) {
-        Glide.with(this).load(path).error(R.drawable.default_avatar).centerCrop().into(userPhoto)
+    private fun chargeUserPhoto(uri: Uri) {
+        Glide.with(this).load(uri).error(R.drawable.default_avatar).centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(userPhoto)
+    }
+
+    private fun chargeUserPhoto(path: String) {
+        Glide.with(this).load(path).error(R.drawable.default_avatar).centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(userPhoto)
+    }
+
+    private fun updatePhotoUrl(url: String) {
+        photoUrlMock = url
+//        TODO Atualizar no backend
     }
 }
